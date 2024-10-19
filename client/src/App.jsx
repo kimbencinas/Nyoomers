08ca8259f0f557a90dacc373c7eb73e6d1eab97a
Kimberly Encinas <kimbencinas@gmail.com>
2024-10-18 19:23:22 -0700

    Organize file structure

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import './styles/App.css'
+import Navbar from './components/Navbar'
+
+function App() {
+
+  return (
+    <>
+    <Navbar />
+      homepage, i guess.
+    </>
+  )
+}
+
+export default App
