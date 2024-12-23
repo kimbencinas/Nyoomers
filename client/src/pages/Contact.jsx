d2808a2da95d178ff54a710ea43d78b2e1cd7f51
Kimberly Encinas <kimbencinas@gmail.com>
2024-12-22 23:55:17 -0800

    work on contact page form

--- client/src/pages/Contact.jsx
+++ client/src/pages/Contact.jsx
@@ -1,7 +1,19 @@
+import React, { useState } from "react";
 import Navbar from "../components/Navbar"
 import '../styles/Contact.css'
 
 function Contact() {
+  const [formData, setFormData] = useState({
+    first: '',
+    last: '',
+    email: '',
+    message: ''
+  });
+
+  const handleInputChange = (e) => {
+    const { id,value } = e.target;
+    setFormData({ ...formData, [id]: value });
+  };
 
     return (
       <div>
@@ -9,10 +21,37 @@ function Contact() {
         <div className="contact-container">
           <h2>Get In Touch</h2>
           <form className="contact-form">
-            <input type="text" id="first-name" placeholder="First Name" required />
-            <input type="text" id="last-name" placeholder="Last Name" required />
-            <input type="text" id="email" placeholder="Email Address" required />
-            <textarea id="message" placeholder="Message" required></textarea>
+            <input 
+              type="text" 
+              id="first-name" 
+              placeholder="First Name" 
+              value={formData.first}
+              onChange={handleInputChange}
+              required 
+            />
+            <input 
+              type="text" 
+              id="last-name" 
+              placeholder="Last Name" 
+              value={formData.last}
+              onChange={handleInputChange}
+              required 
+            />
+            <input 
+              type="text" 
+              id="email" 
+              placeholder="Email Address" 
+              value={formData.email}
+              onChange={handleInputChange}
+              required 
+            />
+            <textarea 
+              id="message" 
+              placeholder="Message" 
+              value={formData.message}
+              onChange={handleInputChange}
+              required>
+            </textarea>
             <button type="submit">Send Message</button>
           </form>
         </div>
