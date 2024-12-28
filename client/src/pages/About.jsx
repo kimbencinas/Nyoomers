cf55456afba32bc06bd6875cd19277949504198f
Kimberly Encinas <kimbencinas@gmail.com>
2024-12-27 16:27:34 -0800

    Add section to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -8,7 +8,10 @@ function About() {
       <div>
         <div>
           <h2>About Nyoomers</h2>
-          <p>Coming soon.</p>
+          <p>Welcome to Nyoomers, your trusted source for affordable electric and hybrid vehicles in California's Central Valley. We are 
+            proud to offer a wide selection of eco-friendly cars designed to fit your budget and lifestyle. Our mission is to make 
+            sustainable driving accessible to everyone, with a focus on providing vehicles that are both economical and environmentally
+            responsible.</p>
         </div>
       </div>
     </>
