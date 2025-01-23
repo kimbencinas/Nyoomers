cc0955619cd70e476a9a77b000ea4b054c53f734
Kimberly Encinas <kimbencinas@gmail.com>
2025-01-22 22:27:22 -0800

    Added more to about page

--- client/src/pages/About.jsx
+++ client/src/pages/About.jsx
@@ -7,12 +7,15 @@ function About() {
       <Navbar />
       <div>
         <div>
+          <h2>Our Story</h2>
+          <p>Making electric vehicles accessible to everyone in California.</p>
           <h2>How We Started</h2>
           <p>Welcome to Nyoomers, your trusted source for affordable electric and hybrid vehicles in California's Central Valley. We are 
             proud to offer a wide selection of eco-friendly cars designed to fit your budget and lifestyle. Our mission is to make 
             sustainable driving accessible to everyone, with a focus on providing vehicles that are both economical and environmentally
             responsible.</p>
-          <p>Our mission was clear from day one: make eco-friendly transportation accessible to all Californians by offering quality pre-owned electric vehicles under $10,000. We started with just five cars in a small lot, but our commitment to affordability and sustainability quickly resonated with the community.</p>
+          <p>Our mission was clear from day one: make eco-friendly transportation accessible to all Californians by offering quality pre-owned 
+            electric vehicles under $10,000. </p>
         </div>
       </div>
     </>
